2792d7219c2acf226b346626071f8ae646ead618
1200966 <gaspardiogo55@gmail.com>
2024-06-08 19:56:16 +0100

    impr: add roles to extractAuthorities

--- resource_server/src/main/java/com/commerce/backend/security/SecurityConfiguration.java
+++ resource_server/src/main/java/com/commerce/backend/security/SecurityConfiguration.java
@@ -4,13 +4,14 @@ import com.commerce.backend.constants.SecurityConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
